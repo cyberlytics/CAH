26e06fbeb384d4093c3fc53471dae7089829c9ba
Christoph Boni <c.boni@oth-aw.de>
2022-05-27 21:52:57 +0200

    Adds Basic Routing and Communication with Backend

---
+++ sys-src/Backend/Server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require("socket.io");
+
+app.use(express.urlencoded({extended: true}));
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+    },
+});
+
+let clientNo = 0;
+
+io.on("connection", (socket) => {
+    console.log(`User Connected: ${socket.id}`);
+    clientNo++;
+
+    //socket.join(Math.round(clientNo/2));
+    //socket.emit('serverMsg', roomNo);
+
+    console.log(`User Anzahl: ${clientNo}`);
+    socket.on("join_room", (data) => {
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    })
+    socket.on("disconnect", () => {
+        clientNo--;
+        console.log(`User Anzahl: ${clientNo}`);
+        console.log("User Disconnected", socket.id);
+    })
+});
+
+server.listen(3001 , () => {
+    console.log("server running")
+});
--- sys-src/Frontend/src/App.js
+++ sys-src/Frontend/src/App.js
@@ -1,24 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import Room from './pages/Room';
+import Startpage from './pages/startpage';
+import Lobby from './pages/Lobby';
+import {Link, BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <Router>
+    <Routes>
+      <Route path ="/" element={<Startpage/>}/>
+      <Route path ="/Room" element={<Room/>}/>
+      <Route path="/Lobby" element={<Lobby/>}/>
+      <Route path ="*" element={<Startpage/>}/>
+    </Routes>
+  </Router>
+  
   );
 }
 
