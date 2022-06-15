5dec7ec2be30966ce85ed0e9f23535d3773fcaf6
Fabian Kraus <f.kraus1@oth-aw.de>
2022-06-15 19:51:57 +0200

    Playerausgabe funktioniert

--- sys-src/Frontend/src/pages/Lobby.js
+++ sys-src/Frontend/src/pages/Lobby.js
@@ -1,129 +1,81 @@
-import React from 'react';
-import io, { Socket } from 'socket.io-client';
-import {useNavigate, useParams} from 'react-router-dom';
-import Button from 'react-bootstrap/Button';
-import {Col, Row, Container} from 'react-bootstrap'
-import BlackCard from '../components/BlackCard.js';
-import WhiteCard from '../components/WhiteCard.js';
+import React from "react";
+import io, { Socket } from "socket.io-client";
+import { useNavigate, useParams } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { Col, Row, Container } from "react-bootstrap";
+import BlackCard from "../components/BlackCard.js";
+import WhiteCard from "../components/WhiteCard.js";
 import { useState, useEffect } from "react";
-import UserContextProvider from '../contexts/UserContext.js';
+import UserContextProvider from "../contexts/UserContext.js";
 
 function Lobby(props) {
   const [roomsize, setRoomsize] = useState(0);
-  const [spieler, setSpieler] = useState([]);
-  const [spielerObject, setSpielerObject] = useState([]);
+  const [playerObject, setPlayerObject] = useState([]);
+  const [iscreator, creator] = useState(false);
 
+  const [gamestarted, started] = useState(false);
+  const [counter, setCounter] = useState(5);
 
+  let navigate = useNavigate();
+  let navbuttons = [{ Function: "startGame", Text: "Start Game" }];
 
-    
-    let navigate = useNavigate();
-    const [gamestarted, started] = useState(false);
-    const [iscreator, creator] = useState(false);
-    const [counter, setCounter] = useState(5);
-
-
-    let roomsize;
-    // hier creator prop einfügen in spieler
-    let spieler = [];
-    let navbuttons = [
-      {"Function": "startGame", "Text": "Start Game"},
-    ]
-
-    // Checkt, ob der Spielstarte button gedrückt wurde und löst dann einen counter aus, der bei null den view zu game wechselt
-    useEffect(() => {
-      if(gamestarted){
+  // Checkt, ob der Spielstarte button gedrückt wurde und löst dann einen counter aus, der bei null den view zu game wechselt
+  useEffect(() => {
+    if (gamestarted) {
       const timer =
         counter > 0 && setTimeout(() => setCounter(counter - 1), 1000);
-      }
-      if(counter === 0){
-        navigate("/Game");
-      }
-    }, [counter, gamestarted]);
+    }
+    if (counter === 0) {
+      navigate("/Game");
+    }
+  }, [counter, gamestarted]);
 
 
-    
-      props.Socket.on("userLeavesLobby", (gameobject, size)=>{
-        roomsize = size;
-        spieler.forEach(element => {
-          if(!gameobject.players.includes(element.player)){
-            const index = spieler.indexOf(element.player)
-            spieler.splice(index, 1);
-          }
-        });
-        //update nachdem ein user den raum verlasesn hat
-        console.debug(`Die User ${spieler} treffen ein und die Raumbelegung ${roomsize} von 5`)
-      })
+  props.Socket.on("updateLobby", (gameobject, size) => {
+    setRoomsize(size);
+    setPlayerObject(gameobject.players);
+    // console.log("SpielerObject: ", playerObject);
 
-      /*props.Socket.on("creatorJoinsLobby", (gameobject, size) =>{
-        roomsize = size;
-        gameobject.players.forEach(element => {
-          if(!spieler.includes(element.player)){
-            spieler.push(element.player);
-          }
-        });
-        // hier werden dem aktuellen client alle spieler und die raumkapazität gegeben
-        console.debug(`Die User ${spieler} treffen ein und die Raumbelegung ${roomsize} von 5`)
-      });*/
+    var creatorr = gameobject.players.find((player) => player.creator);
+    if (creatorr.socket == props.Socket.id) {
+      creator(true);
+    }
 
-      var aufrufecounter = 0;
-    
-      //komplizierter muss noch richtig gelöst werden
-      props.Socket.on("userJoinsLobby", (gameobject, size) =>{
-        aufrufecounter++;
-        console.log(aufrufecounter);
-        console.debug("Debug test");
-        roomsize = size;
-       
-        gameobject.players.forEach(element => {
-          if(!spieler.includes(element.player)){
-            spieler.push(element.player);
-          }
-        });
-        var creatorr = gameobject.players.find(player => player.creator)
-        console.log(creatorr);
-        if(creatorr.socket == props.Socket.id){
-          console.log("creator")
-          creator(true);
-        }
-        // hier werden dem aktuellen client alle spieler und die raumkapazität gegeben
-        console.debug(`Die User ${spieler} treffen ein und die Raumbelegung ${roomsize} von 5`)
-      });
+    // hier werden dem aktuellen client alle spieler und die raumkapazität gegeben
+    console.debug(
+      `Die User ${playerObject} treffen ein und die Raumbelegung ${roomsize} von 5`
+    );
+  });
 
-      
-      props.Socket.on("creatorStartsGame", () => {
-        started(true);
-    })
+  props.Socket.on("creatorStartsGame", () => {
+    started(true);
+  });
 
-    
-
-    return(
-        <Container fluid className=" vh-100">
-            { gamestarted && (<div className="text-center startcounter position-absolute">
-                            {counter}
-                           </div>
-                           )
-            }
-            <Row className="vh-100">
-            <Col>
-                <BlackCard/>
-            </Col>
-            <Col>
-                    <WhiteCard Socket={props.Socket} TextFields={spieler}/>
-            </Col>
-            <Col>
-                    <WhiteCard/>
-            </Col>
-            {iscreator && (<Col>
-                    <WhiteCard Socket={props.Socket} NavigateButtons={navbuttons}/>
-                    
-                
-            </Col>)}
-            
-            
-            
-            </Row>
-        </Container>
-    )
+  return (
+    <Container fluid className=" vh-100">
+      {gamestarted && (
+        <div className="text-center startcounter position-absolute">
+          {counter}
+        </div>
+      )}
+      <Row className="vh-100">
+        <Col>
+          <BlackCard />
+        </Col>
+        <Col>
+          <WhiteCard Socket={props.Socket} TextFields={playerObject} />
+        </Col>
+        <Col>
+          <WhiteCard />
+        </Col>
+        {iscreator && (
+          <Col>
+            <WhiteCard Socket={props.Socket} NavigateButtons={navbuttons} />
+          </Col>
+        )}
+      </Row>
+    </Container>
+  );
 }
 
 export default Lobby;
