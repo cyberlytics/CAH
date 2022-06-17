7e057595f955e4de39588d56d95196bf07065bec
Jean-Pierre Afara <j.afara@oth-aw.de>
2022-06-17 16:53:51 +0200

    Hinzufügen des Spielbrettes
    Former-commit-id: cec8cae6f5ece8223dd065376180aa49552bd588

---
+++ sys-src/Frontend/src/pages/gamepage.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import io, { Socket } from 'socket.io-client';
+import {useNavigate, useParams} from 'react-router-dom';
+import {Col, Row, Container} from 'react-bootstrap';
+import BlackCard from '../components/BlackCard.js';
+import WhiteCardStack from '../components/WhiteCardStack.js';
+import UserContextProvider from '../contexts/UserContext.js';
+import WhiteCard from '../components/WhiteCard.js';
+
+function GamePage(){
+
+    return (
+        <Container fluid className="vh-100">
+            <Row className='vh-100'>
+                <Col><BlackCard/></Col>
+                <Col></Col>
+                <Col></Col>
+                <Col></Col>
+                <Col className = "playerlist">
+                    
+                    
+                </Col>
+            </Row>
+          <Row className ="vh-100">
+                <Col><WhiteCard/></Col>
+                <Col><WhiteCard/></Col>
+                <Col><WhiteCard/></Col>
+                <Col><WhiteCard/></Col>
+                <Col><WhiteCard/></Col>
+          </Row>
+          
+        </Container>
+       
+    )
+}
+
+export default GamePage;
