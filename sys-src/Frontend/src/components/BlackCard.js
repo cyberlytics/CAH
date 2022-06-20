97bc3e1a56f57a79b3acdc25bf7bf25503a7e65b
Jean-Pierre Afara <j.afara@oth-aw.de>
2022-06-20 21:48:22 +0200

    Ausgeben der Spieler Hand
    Former-commit-id: 8ba86843b0f96d504512cfccdf14d2c15570fd57

--- sys-src/Frontend/src/components/BlackCard.js
+++ sys-src/Frontend/src/components/BlackCard.js
@@ -10,7 +10,8 @@ function BlackCard(props){
     return (
         <div className="justify-content-center flex-column d-flex heightchanger blackdiv h-100">
                     <p className="gametitle text-white">{props.title}</p>
-            
+                    {//TODO: Ändere Size für BlackCard Inhalt für das GameField
+                    }
         </div>
     )
 }
