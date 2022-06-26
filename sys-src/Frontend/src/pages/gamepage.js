651c146508a69ff773db4c894a53e70e8d64f3a1
Jean-Pierre Afara <cyrisk@MacBook-Pro-von-Jean-Pierre.local>
2022-06-26 09:40:50 +0200

    Anzeigen der ausgewählten Karten

--- sys-src/Frontend/src/pages/gamepage.js
+++ sys-src/Frontend/src/pages/gamepage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import io, { Socket } from 'socket.io-client';
 import {useNavigate, useParams} from 'react-router-dom';
 import {Col, Row, Container, Button} from 'react-bootstrap';
@@ -14,16 +14,27 @@ function GamePage(props){
 
 const [blackCard, setBlackCard] = useState([]);
 const [playerHand, setPlayerHand] = useState([]);
+const [roundIsPlayed, setRoundIsPlayed] = useState(false); 
+const [playedRound, updatePlayedRound] = useState([]);
+const [playerCount, setPlayerCount] = useState();
 
 return (
     <UserContext.Consumer>
     {(context) => {
         const { userName, userRoom } = context;
 
+props.Socket.on("UpdateDisplayedWhiteCards", (chosenCards) => {
+    console.log(chosenCards[0].currWhiteCard);
+    setRoundIsPlayed(true);
+    updatePlayedRound(chosenCards);
+
+
+});
+
 props.Socket.on("push_gameobject", (gameObject) =>{
     console.log(gameObject);
     setBlackCard(gameObject.currBlackCard);
-
+    setPlayerCount(gameObject.players.length)
     gameObject.players.forEach(element=>{
         if(element.socket == props.Socket.id) {
             setPlayerHand(element.hand);
@@ -33,14 +44,36 @@ props.Socket.on("push_gameobject", (gameObject) =>{
 });
 
 
+
+
+
+
+
+
     return (
         <Container fluid className="vh-100">
             <Row className='centerField'>
                 <Col><BlackCard title = {blackCard.Inhalt}/></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
-                <Col></Col>
+                <Col>
+                    {roundIsPlayed && (playerCount >=2 ) && (
+                    <WhiteCard reCards={playedRound[1].currWhiteCard}></WhiteCard>
+                    )}
+                </Col>
+                <Col>
+                {roundIsPlayed && (playerCount  >= 3) && (
+                    <WhiteCard reCards={playedRound[2].currWhiteCard}></WhiteCard>
+                    )}
+                </Col>
+                <Col>
+                    {roundIsPlayed && (playerCount >= 4) && (
+                    <WhiteCard reCards={playedRound[3].currWhiteCard}></WhiteCard>
+                    )}
+                </Col>
+                <Col>
+                    {roundIsPlayed && (playerCount >= 5) && (
+                    <WhiteCard reCards={playedRound[4].currWhiteCard}></WhiteCard>
+                    )}
+                </Col>
             </Row>
 
           <Row className ="playerHand">
