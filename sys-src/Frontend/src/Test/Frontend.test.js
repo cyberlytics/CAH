9f49e132a8f2703deb3781cdf075f08ec813a291
Spies Sophie <s.spies@oth-aw.de>
2022-06-27 10:16:41 +0200

    added tests,frontend

--- sys-src/Frontend/src/Test/Frontend.test.js
+++ sys-src/Frontend/src/Test/Frontend.test.js
@@ -13,13 +13,13 @@ import compNavButton from "../components/NavigateButton";
 import comTextfield from "../components/TextField";
 import comUserText from "../components/UserTextInputs";
 import UserContextProvider, {UserContext} from "../contexts/UserContext";
-const React=require("react");
+import React from "react";
+import {Container} from "react-bootstrap";
+import {create} from "react-test-renderer";
 const { unmountComponentAtNode,render } =require( "react-dom");
-const {act}=require("react-dom/test-utils");
-import { createRoot } from 'react-dom/client';
-import {changeAttribute} from "jsdom/lib/jsdom/living/attributes";
-const container = document.createElement("div");
 
+
+const container = document.createElement("div");
 // Create a root.
 const root = ReactDOMClient.createRoot(container);
 
@@ -34,21 +34,12 @@ it("renders with or without a name", () => {
 
     expect(1).toBe(1);
 });
-  //  it("Lobby test",()=>{
-  // expect(lobby.name).not.toBeDefined();
-  //  });
- //   it("Lobby test 2",()=>{
-   //     root.render(<Lobby probs="random" />,container)
-  //  expect(container.textContent).toBeDefined();
-//});
+
     it("test Black Card",()=>{
         const props="random";
 expect(blackCard(props)).not.toBeDefined();
     });
-//it("test startpage",()=>{
- //   const props="random";
-  //  expect(startpage(props)).toBeUndefined();
-//});
+
 it(' components/whitecard',()=>{
     const props="random";
     expect(compWhitecard(props)).toBeDefined();
@@ -74,7 +65,29 @@ it("Test Class UserContextProvider",()=>{
 
     expect(UserContext).toBeDefined();
 })
-//it("test Components NavigateButton",()=>{
-//    const props="random";
-//    expect(compNavButton(props)).toBeDefined();
-//})
+//not working becauseof react
+it("Lobby test ",()=>{
+
+    const root =create(<Lobby />).root;
+  //  unmountComponentAtNode(container);
+    root.render(<Lobby props="random"/>,container)
+    expect(container.textContent).toBeDefined();
+    root.unmount();
+});
+it("Lobby test 2",()=>{
+    let navigate = useNavigate();
+    const root =create(<Lobby navigate/>).root;
+    const element = root.findByType("div");
+
+    expect(element.name).toBeDefined();
+
+});
+
+it("test Components NavigateButton",()=>{
+    const props="random";
+    expect(compNavButton(props)).toBeDefined();
+})
+it("test startpage",()=>{
+    const props="random";
+    expect(startpage(props)).toBeUndefined();
+});
