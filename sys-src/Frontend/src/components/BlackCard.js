fdbb976de5af6c33df76373609a3edd760b018b7
Jonas Bachmeier <j.bachmeier@oth-aw.de>
2022-06-27 09:18:50 +0200

    CSS anpassungen
    Gametitle Größe angepasst und Lobby-Titel hinzugefügt

--- sys-src/Frontend/src/components/BlackCard.js
+++ sys-src/Frontend/src/components/BlackCard.js
@@ -8,10 +8,8 @@ import {Col, Row, Container} from 'react-bootstrap'
 function BlackCard(props){
 
     return (
-        <div className="justify-content-center flex-column d-flex heightchanger blackdiv h-100">
-                    <p className="gametitle text-white">{props.title}</p>
-                    {//TODO: Ändere Size für BlackCard Inhalt für das GameField
-                    }
+        <div className={`  justify-content-center flex-column d-flex heightchanger blackdiv h-100`}>
+                    <p className={`${props.className} gametitle text-white`}>{props.title}</p>
         </div>
     )
 }
